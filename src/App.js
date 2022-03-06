9fcdd0108eaf795ccf7948ffee8345d59faae1bc
KevvKo <kevin@LAP-09KKL.fritz.box>
2022-03-06 01:11:23 +0100

    update functionallity for several events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import * as React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import ParticipantContext from './context/ParticipantContext';
 
 // Components
 import Tabs from '@mui/material/Tabs';
@@ -11,27 +12,43 @@ import TournamentPlanView from './views/tournamentPlan/tournamentPlanView';
 import ParticipantsView from './views/participants/participantsView';
 
 function App() {
+
   const [value, setValue] = React.useState(0);
+const [ participants, setParticipants ] = useState(
+    [
+        'Eisenhüttenstädter Räuber',
+        'Berliner Kojoten',
+        'Jenenser Flaschen',
+        'Erfurter Assis'
+    ]
+  )
+  const defaultValues = {
+    participants: participants,
+    setParticipants: setParticipants
+  }
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
   return (
     <div className="App">
-      <Box sx={{ width: '100%' }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-            <Tab label="Teilnehmer" {...a11yProps(0)} />
-            <Tab label="Turnierplan" {...a11yProps(1)} />
-          </Tabs>
+      <ParticipantContext.Provider value={defaultValues}>
+        <Box sx={{ width: '100%' }}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+              <Tab label="Teilnehmer" {...a11yProps(0)} />
+              <Tab label="Turnierplan" {...a11yProps(1)} />
+            </Tabs>
+          </Box>
+          <TabPanel value={value} index={0}>
+            <ParticipantsView />
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <TournamentPlanView />
+          </TabPanel>
         </Box>
-        <TabPanel value={value} index={0}>
-          <ParticipantsView />
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <TournamentPlanView />
-        </TabPanel>
-      </Box>
+      </ParticipantContext.Provider>
     </div>
   );
 }
--- src/views/participants/participantsView.js
+++ src/views/participants/participantsView.js
@@ -1,10 +1,91 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import './participantsView';
+import ParticipantContext from "../../context/ParticipantContext";
 // Components
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Typography } from "@mui/material";
+import Snackbar from '@mui/material/Snackbar';
 
 const ParticipantsView = () => {
-    return( <Box component='div'></Box>)
+
+    const { participants, setParticipants } = useContext(ParticipantContext);
+    const [ inputValue, setInputValue ] = useState('');
+    const [ open, setOpen ] = useState(false);
+
+    const handleClickAddParticipant = () => {
+
+        if(inputValue){
+            setParticipants([
+                ...participants,
+                inputValue
+            ])
+        }
+    }   
+
+    const handleChange = (e) => {
+        if( e.target.value ){
+            setInputValue(e.target.value);
+        }
+    };  
+
+    const handleClose = (e, reason) => {
+        if (reason === 'clickaway') {
+            return;
+          }
+        setOpen(false);
+    };
+
+    return( 
+        <Box>
+            <Grid container spacing={3}>
+                <ParticipantContext.Consumer>
+                    { ({ participants, setParticipants }) => (
+                        participants.map((e, i) => {
+
+                            const handleClickRemove = (i) => {
+                                
+                                const update = participants.filter(( e, j) =>  i !== j )
+                                setParticipants(update)
+                                setOpen(true);
+                            }
+
+                            return (
+                                <Grid item xs={12} key={i}>
+                                    <Typography component='span'>
+                                        { e }
+                                    </Typography>
+                                    <Button variant='contained' onClick={ () => { handleClickRemove(i) }}>
+                                        Entfernen
+                                    </Button>
+                                </Grid>
+                            )
+                        })
+                    )}
+                </ParticipantContext.Consumer>
+                <Grid item xs={12} sx={{display: 'flex', alignItems: 'center'}}>
+                    <TextField 
+                        id="outlined-basic" 
+                        label="Vereinsname" 
+                        variant="outlined" 
+                        sx={{marginRight: '25px'}}
+                        onChange={ handleChange }    
+                    />
+                    <Button variant='contained' sx={{height: '40px'}} onClick={ handleClickAddParticipant }>
+                        Hinzufügen
+                    </Button>
+                </Grid>
+            </Grid>
+            <Snackbar
+                open={open}
+                autoHideDuration={6000}
+                onClose={handleClose}
+                message="Teilnehmer entfernt"
+            />
+        </Box>
+    )
 }
 
 export default ParticipantsView;
