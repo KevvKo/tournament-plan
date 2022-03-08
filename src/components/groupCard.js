a0a029374d3804418d064beb957f052119149491
KevvKo <kklein915@gmail.com>
2022-03-08 23:03:04 +0100

    add textfields for groupcards

--- src/components/groupCard.js
+++ src/components/groupCard.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { Box } from '@mui/system';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import { Grid } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import { TextField } from '@mui/material';
 
 const GroupCard = (props) => {
 
@@ -11,20 +13,29 @@ const GroupCard = (props) => {
     
     const participantList = group.map((e) => {
         return (
-            <Typography align='left' component='span' sx={{marginRight: '15px'}}>
-                {e}
-            </Typography>
+            <Grid item sx={{display: 'flex', alignItems: 'center'}}>
+                <Typography align='left' component='span' variant="body2" sx={{marginRight: '15px'}}>
+                    {e}
+                </Typography>
+                <TextField type='number' size="small" defaultValue={0} sx={{ width: '70px', marginLeft: 'auto   '}}/>
+            </Grid>
         )
     })
 
     return(
-        <Card className='group-card' variant="outlined" sx={{ marginBottom: '70px', maxWidth: '350px'}}>
+        <Card className='group-card' variant="outlined" sx={{ marginBottom: '70px', width: 'fit-content'}}>
             <CardContent>
                 <Typography gutterBottom variant="h6" align="left" >
                     { `Group ${groupId}` }
                 </Typography>
                 <Box sx={{display: 'inline'}}>
-                   {participantList}
+                    <Grid container>
+                        <Grid item>
+                            <Grid container direction='column'>
+                                {participantList}
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </Box>
             </CardContent>
         </Card>
