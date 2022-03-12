017535555854a9f844bfc9f984952ae03c2da9af
KevvKo <kklein915@gmail.com>
2022-03-13 00:40:47 +0100

    setup build

--- public/main.js
+++ public/main.js
@@ -1,39 +1,33 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
 const isDev = require("electron-is-dev");
+let mainWindow;
 
-const createWindow = () => {
-    const window = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
+function createWindow() {
+    mainWindow = new BrowserWindow({ width: 900, height: 680 });
+    
+    mainWindow.loadURL(
+      isDev 
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+    );
+      
+      mainWindow.on("closed", () => (mainWindow = null));
+
+      if (isDev) {
+        mainWindow.webContents.openDevTools({ mode: "detach" });
       }
-    });
-  
-    const startUrl = process.env.ELECTRON_START_URL || url.format({
-      pathname: path.join(__dirname, '/../build/index.html'),
-      protocol: 'file:',
-      slashes: true
-    });    
-  
-    window.loadURL( startUrl );
-  
-    // Open the DevTools.
-    if (isDev) {
-      window.webContents.openDevTools({ mode: "detach" });
+    
     }
-  };
 
-  app.whenReady().then(() => {
-      createWindow();
+  
+    app.on( "ready", createWindow);
+    app.on("window-all-closed", () => {
+      if (process.platform !== "darwin")
+        {app.quit();}
+      }
+    );
 
-      app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-      });
-  });
-
-  // dont close app, if os is macOs
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
-  });
+    app.on("activate", () => {if (mainWindow === null) {createWindow();}});
--- src/utilitys.js
+++ src/utilitys.js
@@ -1,121 +1,95 @@
-module.exports = {
 
-    /**
-     * @summary receiving a list of participants for the tournament and 
-     * assigning them into into stages and groups
-     * @param {*} participants 
-     * @returns {Array}
-     */
-    createGroups: ( participants ) => {
-        
-        const tournamentPlan = [];
-        const groupStage = []
-        let group = [];
 
-        participants.forEach( participant => {
-            group.push({
-                name: participant, 
-                score: 0
-            });
+/**
+ * @summary receiving a list of participants for the tournament and 
+ * assigning them into into stages and groups
+ * @param {*} participants 
+ * @returns {Array}
+ */
+export const createGroups = ( participants ) => {
+    
+    const tournamentPlan = [];
+    const groupStage = []
+    let group = [];
 
-            if( group.length === 4 ){
-                groupStage.push(group)
-                group = [];
-            }
+    participants.forEach( participant => {
+        group.push({
+            name: participant, 
+            score: 0
         });
 
-        if(group.length > 0) groupStage.push(group);
+        if( group.length === 4 ){
+            groupStage.push(group)
+            group = [];
+        }
+    });
 
-        // push the first groupstage
-        tournamentPlan.push(groupStage);
-        
-        // for(let i = groupStage.length; i >= 1; i=i/2){
-        //     console.log(i)
-        //     const stage = Array( Math.round(i)).fill([])
-        //     tournamentPlan.push(stage);
-        // }
+    if(group.length > 0) groupStage.push(group);
 
-        return tournamentPlan;
-    },
+    // push the first groupstage
+    tournamentPlan.push(groupStage);
+    
+    // for(let i = groupStage.length; i >= 1; i=i/2){
+    //     console.log(i)
+    //     const stage = Array( Math.round(i)).fill([])
+    //     tournamentPlan.push(stage);
+    // }
 
-    /**
-     * @summary reveices a data structure, presenting the tournament, and
-     * assigning the candidates according to there scores to the new stage into groups
-     * @param {Array} tournamentData 
-     * @returns {Array} 
-     */
+    return tournamentPlan;
+}
 
-    assignNewStage: ( tournamentData ) => {
+/**
+ * @summary reveices a data structure, presenting the tournament, and
+ * assigning the candidates according to there scores to the new stage into groups
+ * @param {Array} tournamentData 
+ * @returns {Array} 
+ */
 
-            // find the first stage from the tournament, where the first group hast
-            // no member. Refers this stage has no me   mbers currently
-            const stagesCount = tournamentData.length
-            const stage = tournamentData[ stagesCount - 1 ];
-            const newStage = [];
+export const assignNewStage = ( tournamentData ) => {
 
-            // loop through the previous stage to assign the first and second placed winner
-            // cross for the next stage for the second stage
-            if( stagesCount === 1 ){
-                for( let j = 0, k = stage.length; j < k; j++){
+        // find the first stage from the tournament, where the first group hast
+        // no member. Refers this stage has no me   mbers currently
+        const stagesCount = tournamentData.length
+        const stage = tournamentData[ stagesCount - 1 ];
+        const newStage = [];
 
-                    const group = [...stage[j]];
-                    const oppositeGroup = [...stage[ k - j - 1]]
+        // loop through the previous stage to assign the first and second placed winner
+        // cross for the next stage for the second stage
+        if( stagesCount === 1 ){
+            for( let j = 0, k = stage.length; j < k; j++){
 
-                    // create the first stage cross assignment
-                    const candidate1 = {...group.sort(( a,b ) => b.score - a.score )[0]};
-                    const candidate2 = {...oppositeGroup.sort(( a,b ) => b.score - a.score)[1]}
-                    
-                    candidate1.score = 0;
-                    candidate2.score = 0;
+                const group = [...stage[j]];
+                const oppositeGroup = [...stage[ k - j - 1]]
 
-                    newStage.push([
-                        candidate1,
-                        candidate2
-                    ]) 
-                }
+                // create the first stage cross assignment
+                const candidate1 = {...group.sort(( a,b ) => b.score - a.score )[0]};
+                const candidate2 = {...oppositeGroup.sort(( a,b ) => b.score - a.score)[1]}
+                
+                candidate1.score = 0;
+                candidate2.score = 0;
 
-                tournamentData.push(newStage);
-                return tournamentData;
-            } 
-
-            if( stage.length % 2 === 0){
-                // assigning the remaining candidates for a even count of groups
-                for( let j = 0, k = stage.length; j < k/2; j++){
-                    const group = [...stage[j]];
-                    const oppositeGroup = [...stage[ k - j - 1]];
-
-                    const candidate1 = {...group.sort(( a,b ) => b.score - a.score)[0]};  
-                    const candidate2 = {...oppositeGroup.sort(( a,b ) => b.score - a.score )[0]};
-            
-                    candidate1.score = 0;
-                    candidate2.score = 0;
-
-                    newStage.push([
-                        candidate1,
-                        candidate2
-                    ]) 
-                };
-
-                tournamentData.push(newStage);
-                return tournamentData;
+                newStage.push([
+                    candidate1,
+                    candidate2
+                ]) 
             }
 
-            // assigning the remaining candidates if the count of group is odd
-            for( let j = 0, k = stage.length; j <= (k-1)/2; j++){   
-                console.log(stage)
+            tournamentData.push(newStage);
+            return tournamentData;
+        } 
+
+        if( stage.length % 2 === 0){
+            // assigning the remaining candidates for a even count of groups
+            for( let j = 0, k = stage.length; j < k/2; j++){
                 const group = [...stage[j]];
                 const oppositeGroup = [...stage[ k - j - 1]];
 
-                if( j === (k-1)/2 ){
-                    newStage.push(group);
-                    continue
-                }
-                const candidate1 = {...group.sort(( a,b ) =>  b.score - a.score )[0]};  
+                const candidate1 = {...group.sort(( a,b ) => b.score - a.score)[0]};  
                 const candidate2 = {...oppositeGroup.sort(( a,b ) => b.score - a.score )[0]};
-    
+        
                 candidate1.score = 0;
                 candidate2.score = 0;
-                
+
                 newStage.push([
                     candidate1,
                     candidate2
@@ -124,6 +98,32 @@ module.exports = {
 
             tournamentData.push(newStage);
             return tournamentData;
-        
-    }
+        }
+
+        // assigning the remaining candidates if the count of group is odd
+        for( let j = 0, k = stage.length; j <= (k-1)/2; j++){   
+            console.log(stage)
+            const group = [...stage[j]];
+            const oppositeGroup = [...stage[ k - j - 1]];
+
+            if( j === (k-1)/2 ){
+                newStage.push(group);
+                continue
+            }
+            const candidate1 = {...group.sort(( a,b ) =>  b.score - a.score )[0]};  
+            const candidate2 = {...oppositeGroup.sort(( a,b ) => b.score - a.score )[0]};
+
+            candidate1.score = 0;
+            candidate2.score = 0;
+
+            newStage.push([
+                candidate1,
+                candidate2
+            ]) 
+        };
+
+        tournamentData.push(newStage);
+        return tournamentData;
+    
 }
+
