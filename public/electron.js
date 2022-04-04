fcdb0678959c336267a9c3526ce7511f0569a197
KevvKo <kklein915@gmail.com>
2022-04-04 23:21:46 +0200

    apply logo

--- public/electron.js
+++ public/electron.js
@@ -3,20 +3,17 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require("path");
 const isDev = require("electron-is-dev");
-
 let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({ 
-      width: 900, height: 680,
-      icon:`file://${path.join(__dirname, '../src/assets/logo.jpeg')}`
+      width: 900, height: 680
     });
 
-    mainWindow.setIcon(path.join(__dirname, '../src/assets/logo.jpeg'));
     mainWindow.loadURL(
       isDev 
       ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "/logo.jpeg")}`
+      : `file://${path.join(__dirname, '../build/index.html')}`
     );
       
       mainWindow.on("closed", () => (mainWindow = null));
